0b6005ca1a16c8197a14b87103520eaa65decff9
Egrassa <bacek90@mail.ru>
2021-10-24 20:17:17 +0500

    Add prime game

---
+++ bin/games/brain-prime.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import engine from '../src/index.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+const genQuestionAndAnswer = () => {
+  const number = _.random(1, 250);
+  let answer;
+  if (isPrime(number)) {
+    answer = 'yes';
+  } else {
+    answer = 'no';
+  }
+  return {
+    question: number,
+    answer: answer,
+  };
+};
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+engine(rule, genQuestionAndAnswer);
